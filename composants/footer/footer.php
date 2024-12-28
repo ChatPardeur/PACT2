0803483e5bbdbb3b1958810c3f1152c8d7187ca2
ChatPardeur <152877668+ChatPardeur@users.noreply.github.com>
2024-12-28 17:55:23 +0100

    Add files via upload

---
+++ composants/footer/footer.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page avec Footer Fixé</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Assurez-vous de lier votre fichier CSS -->
+</head>
+<body>
+
+    <footer class="footer">
+        <div class="footer-container">
+            
+            <div class="footer-section">
+                <h4>A propos</h4>
+                <p>Nous sommes une entreprise dédiée à offrir les meilleurs services à nos clients. Notre mission est de garantir la satisfaction et l'excellence à chaque étape.</p>
+            </div>
+            
+            <div class="footer-section">
+                <h4>Liens utiles</h4>
+                <ul>
+                    <li><a href="accueil.php">Accueil</a></li>
+                    <li><a href="recherche.php">Recherche</a></li>
+                    <li><a href="avis.php">Avis</a></li>
+                    <li><a href="<?php
+                        if($user)
+                        {
+                            echo 'compte.php';              
+                        } else {
+                            echo 'connexion.php';
+                        }
+                    ?>">Profil</a></li>
+                </ul>
+            </div>
+            
+            <div class="footer-section">
+                <h4>Contact</h4>
+                <p> 12 Rue de l'alma , Rennes, Bretagne</p>
+                <p> +33 1 23 45 67 89</p>
+            </div>
+            
+            <div class="footer-section">
+                <h4>Suivez-nous</h4>
+                <a href="https://www.facebook.com"> <img src="/images/Réseaux/facebook.png" /> </a>
+                <a href="https://www.instagram.com"> <img src="/images/Réseaux/instagram.png" /> </a>
+                <a href="https://www.youtube.com"><img src="/images/Réseaux/youtube.png" /> </a>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <p>&copy; 2024 PACT. Tous droits réservés.</p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ composants/header/header.php
@@ -0,0 +1,132 @@
+
+<?php
+    // Inclusion du script pour vérifier si l'utilisateur a un compte pro
+    include('../php/verif_compte_pro.php');
+    include('../php/verif_compte_membre.php');
+?>
+    
+    <link rel="stylesheet" href="../style/style.css">
+    <header class="headerPC-Tab <?php
+        if($comptePro)
+        {
+            echo "navBarPro";               //classe qui réduit la police (car il y a une brique de navigation de plus)
+        }
+    ?>">
+        <nav class="navHeader">
+            <a id="logoEtPro" href="/pages/accueil.php">
+                <img class="logoHeader" src="/images/logo/logo_grand.png" alt="logo PACT">
+                <?php
+                    if($comptePro)
+                    {
+                        ?>
+                        <p id="textPro">Pro</p>
+                        <?php
+                    }
+                ?>
+            </a>
+
+            <ul class="ulHeader">
+                <li class="liHeader" id="btAccueil">
+                    <a class="aHeader" href="/pages/accueil.php">
+                    <img src="/icones/homeSVG.svg" alt="icone home">
+                    <h3>Accueil</h3>
+                    </a>
+                </li>
+                <li class="liHeader" id="btRech">
+                    <a class="aHeader" href="recherche.php">
+                    <img src="/icones/rechercherSVG.svg" alt="icone rechercher">
+                    <h3>Rechercher</h3>
+                    </a>
+                </li>
+                <?php if($comptePro || $compteMembre)
+                {
+                    ?>
+                        <li class="liHeader" id="btAvis">
+                            <a class="aHeader" href="avis.php">
+                            <img src="/icones/commentSVG.svg" alt="icone commentaires">
+                            <h3>Mes avis</h3>
+                            </a>
+                        </li>
+                    <?php 
+                } 
+                ?>
+                <li class="liHeader" id="btOffres"    <?php 
+                    if(!$comptePro)
+                    {
+                        echo "hidden";                          //cache la brique "mes offres" si l'utilisateur n'est pas un professionnel
+                    }
+                ?>>
+                    <a class="aHeader" href="/pages/gestionOffres.php">
+                    <img src="/icones/offreSVG.svg" alt="icone offres">
+                    <h3>Gestion des offres</h3>
+                    </a>
+                </li>
+                <li class="liHeader" id="btCompte">
+                    <a class="aHeader" href="/pages/compte.php">
+                    <img src="/icones/compteSVG.svg" alt="icone compte">
+                    <?php
+                        if($comptePro || $compteMembre)
+                        {
+                            ?>
+                            <h3>Mon compte</h3>
+                            <?php
+                        }
+                        else
+                        {
+                            ?>
+                            <h3>Connexion</h3>
+                            <?php
+                        }
+                    ?>
+                    
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header> 
+
+    <header class="headerMobile">
+        <nav class="navHeaderMobile">
+            <ul class="ulHeaderMobile">
+                <li class="liHeaderMobile" id="btAccueilMobile">
+                    <a class="aHeaderMobile" href="/pages/accueil.php">
+                    <img src="/icones/homeSVG.svg" alt="icone home">
+                    <div class="trait"></div>
+                    </a>
+                </li>
+                <li class="liHeaderMobile" id="btRechMobile">
+                    <a class="aHeaderMobile" href="/pages/recherche.php">
+                    <img src="/icones/rechercherSVG.svg" alt="icone rechercher">
+                    <div class="trait"></div>
+                    </a>
+                </li>
+                <li class="liHeaderMobile" id="btAvisMobile">
+                    <a class="aHeaderMobile" 
+                    <?php
+                        if($comptePro || $compteMembre)
+                        {
+                            ?>
+                            href="/pages/avis.php"
+                            <?php
+                        }
+                        else
+                        {
+                            ?>
+                            href="/pages/compte.php"
+                            <?php
+                        }
+                    ?>>
+                    <img src="/icones/commentSVG.svg" alt="icone commentaires">
+                    <div class="trait"></div>
+                    </a>
+                </li>
+                <li class="liHeaderMobile" id="btCompteMobile">
+                    <a class="aHeaderMobile" href="/pages/compte.php">
+                    <img src="/icones/compteSVG.svg" alt="icone compte">
+                    <div class="trait"></div>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <script src="/js/click.js"></script>
